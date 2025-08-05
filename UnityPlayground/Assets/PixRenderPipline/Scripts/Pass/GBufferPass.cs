4fb5c29b35798202d1ab9247d68dc68b882cb1f2
罗勇 <luoyong@luoyongdeMacBook-Air.local>
2025-08-05 11:35:12 +0800

    update

--- UnityPlayground/Assets/PixRenderPipline/Scripts/Pass/GBufferPass.cs
+++ UnityPlayground/Assets/PixRenderPipline/Scripts/Pass/GBufferPass.cs
@@ -45,7 +45,7 @@ namespace PixRenderPipline
 
             if (renderer.asset.enable_TAA)
             {
-                renderer.cmb.GetTemporaryRT(GbufferID_3, renderer.size.x, renderer.size.y, 0, FilterMode.Point, RenderTextureFormat.Default, renderer.colorSpace);
+                renderer.cmb.GetTemporaryRT(GbufferID_3, renderer.size.x, renderer.size.y, 0, FilterMode.Point, RenderTextureFormat.RG32, renderer.colorSpace);
                 renderer.cmb.SetRenderTarget(gbuffersWithMotionVec, EarlyZPass.depthID);
                 renderer.cmb.ClearRenderTarget(false, true, gray);
             }
